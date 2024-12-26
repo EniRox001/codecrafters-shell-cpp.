88eb2562c96b6756199b8f3d98a1052a6bb6f109
enoch <enirox001@gmail.com>
2024-12-26 13:50:16 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
 
 int main() {
+
+  // Shell built-in commands
+  std::vector<std::string> builtIns = {"echo", "exit", "type"};
+
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -18,6 +25,25 @@ int main() {
       std::cout << input.substr(5) << std::endl;
       continue;
     }
+
+    // check for built-in command
+    if (input.find("type ") == 0) {
+      std::string val = input.substr(5);
+
+      bool found = false;
+      for (int i = 0; i < builtIns.size(); i++) {
+        if (val == builtIns[i]) {
+          std::cout << val << " is a shell builtin" << std::endl;
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        std::cout << val << ": not found" << std::endl;
+      }
+      continue;
+    }
+
     std::cout << input << ": command not found" << std::endl;
   }
 }
